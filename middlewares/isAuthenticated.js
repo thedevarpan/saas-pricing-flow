c17a3b467715cb61f1a973da1e3b0682d252308b
arpan <me.arpan122@gmail.com>
2026-02-12 20:21:06 +0530

    setup for render

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -3,7 +3,7 @@
 exports.isAuthenticated = (req, res, next) => {
     if (!req.session.user) {
         req.session.errorMessage = "Please login first";
-        return res.redirect("/login");
+        return res.redirect("/user/login");
     }
     next();
 };
