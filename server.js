cdbd0cdd95c9aae30d6f4c0df6b019769a1e56c7
arpan <me.arpan122@gmail.com>
2026-02-13 17:14:21 +0530

    cookie

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(
         }),
         cookie: {
             httpOnly: true,
-            secure: false, // true in production (HTTPS)
+            secure: true, // true in production (HTTPS)
             maxAge: 1000 * 60 * 60 * 24, // 1 day
         },
     })
